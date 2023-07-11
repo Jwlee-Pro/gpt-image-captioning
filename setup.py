0ff4f55bee3658b69c443e16dd782fa71601430b
Joonwon Lee <jwlee-pro@Joonwons-MacBook-Pro-2.local>
2023-07-11 13:28:53 +0900

    apply cookie-cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Yolov5 + GPT-2/3/4',
+    author='Joonwon Lee',
+    license='MIT',
+)
